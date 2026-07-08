5b6a509eddaf781991584d0ef8c94a16faff7a1b
Lethicia Loyse <rodriguessilvall27@gmail.com>
2026-07-08 17:16:51 -0300

    feat: Criação da branch cadastro e incio da funcionalidade cadastrar :sparkles:

---
+++ cadastrar.py
@@ -0,0 +1,28 @@
+print('Cadastrando filme')
+
+nota = 0
+lista_filmes = []
+
+
+# nome_filme = input('Digite o nome do filme:')
+# lista_filmes.append(nome_filme)
+
+# genero = input('Informe o gênero do filme: \n Ex: Ação|Romance|Drama')
+# lista_filmes.append(genero)
+
+# ano_lancamento = input('Informe o ano de lançamento do filme: ')
+# lista_filmes.append(ano_lancamento)
+
+duracao = input('Informe o tempo de duração do filme. \n obs: Informe em minutos.\n ')
+lista_filmes.append(duracao)
+
+# if nota <1 and nota >5:
+#     print('Nota inválida! \n Escolha uma nota em 1 a 5.')
+
+# sinope = lista_filmes.append()
+
+# Colocar uma mensagem no final informando que deu tudo certo
+# colocar condições caso o usuario não digite algo certo
+# Arrumar os input para ficarem visualmente bonitos
+print(lista_filmes)
+
