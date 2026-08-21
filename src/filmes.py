8cede350703af848bfeaba35b2a7eb9bdee36b46
Giovanalucia <giovanaluciafs@gmail.com>
2026-08-21 19:41:19 -0300

    chore: Atualizando código cadastrar e listar na branch US002_Listar :wrench:

--- src/filmes.py
+++ src/filmes.py
@@ -1,57 +1,59 @@
-lista_filmes = []
 filmes = []
 
 class Filmes:
-    def __init__(self, nome_filme, genero,ano_lancamento, duracao, nota, sinopse):
+    def __init__(self, id_filme, nome_filme, genero, ano_lancamento, duracao, nota, sinopse):
+        self.id = id_filme
         self.nome_filme = nome_filme
         self.genero = genero
-        self.ano_lancamento= ano_lancamento
+        self.ano_lancamento = ano_lancamento
         self.duracao = duracao
         self.nota = nota
         self.sinopse = sinopse
 
-def CadastrarFilme():
-    print('Cadastrando filme')
-    nota = 0
+def gerar_novo_id():
+    if not filmes:
+        return 1
+    return max(filme.id for filme in filmes) + 1
 
-    nome_filme = input('Digite o nome do filme:')
-    lista_filmes.append(nome_filme)
+def CadastrarFilme():
+    print('\n--- Cadastrando filme ---')
+
+    nome_filme = input('Digite o nome do filme: ')
 
     genero = input('Informe o gênero do filme. \n Ex: Ação|Romance|Drama \n')
-    if genero.isalpha() :
-        lista_filmes.append(genero) 
-    else:
-        print('ERRO! Gênero deve ser informado por letras.')
+    if not genero.isalpha():
+         print('ERRO! Gênero deve ser informado por letras.')
 
     ano_lancamento = input('Informe o ano de lançamento do filme: ')
+    if not ano_lancamento.isnumeric():
+         print('ERRO! O ano de lançamento do filme deve ser informado em números.')
 
-    if ano_lancamento.isnumeric() :
-        lista_filmes.append(ano_lancamento) 
-    else:
-        print('ERRO! O ano de lançamento do filme deve ser informado números.')
+    while True:
+        try:
+            duracao = int(input('Informe o tempo de duração do filme em minutos: \n'))
+            break
+        except ValueError:
+            print('ERRO! A duração deve ser informada em números inteiros.')
 
-    duracao = input('Informe o tempo de duração do filme:')
+    while True:
+        try:
+            nota = int(input('Informe a nota do filme de 1 a 5: '))
+            if nota < 1 or nota > 5:
+                print('Nota inválida! Escolha uma nota entre 1 e 5.')
+            else:
+                break
+        except ValueError:
+            print('ERRO! Digite um número válido.')
 
-    if duracao.isnumeric():
-        lista_filmes.append(duracao) 
-    else:
-        print('ERRO! A duração deve ser informada em minutos')
+    sinopse = input('Informe a sinopse do filme (Opcional): \n')
 
-    nota = int(input('Informe a nota do filme de 1 a 5: '))
-    if nota <1 or nota >5:
-        print('Nota inválida! \n Escolha uma nota entre 1 e 5.')
+    id_gerado = gerar_novo_id()
 
-    sinopse = input('Informe a sinopse do filme.\n(Esta opção não é obrigatória!)\n') 
-    lista_filmes.append(sinopse)
+    filme_cadastrado = Filmes(id_gerado, nome_filme, genero, ano_lancamento, duracao, nota, sinopse)
 
-    filme_cadastrado = Filmes(nome_filme, genero, ano_lancamento, duracao, nota, sinopse)
-    lista_filmes.append(filme_cadastrado)
-
-    print(f"Filme '{filme_cadastrado.nome_filme}' cadastrado com sucesso!")
     filmes.append(filme_cadastrado)
 
-    for indice, filme in enumerate(filmes, start=1):
-        print(f'{indice} - {filme.nome_filme}')
+    print(f"Filme '{filme_cadastrado.nome_filme}' cadastrado com sucesso com ID {filme_cadastrado.id}!")
 
 
 def ListarFilmes():
@@ -62,6 +64,7 @@ def ListarFilmes():
     for filme in filmes:
         print(f"""
         {{
+            "ID":{filme.id},
             "Nome":{filme.nome_filme}, 
             "Genêro":{filme.genero}, 
             "Ano de Lançamento":{filme.ano_lancamento}, 
@@ -71,54 +74,5 @@ def ListarFilmes():
         }}
             """)
 
-def FiltrarFilmes():
-
-    if not filmes:
-        print("Nenhum filme cadastrado.")
-        return
-
-    print("==== Filtrar por ====")
-    print("[1] Gênero")
-    print("[2] Ano")
-
-    escolha = input("Escolha uma opção: ")
-
-    if escolha == "1":
-        filtro = input("Digite o gênero do filme: ").strip().lower()
-
-        resultados = [
-            filme for filme in filmes
-            if filme.genero.strip().lower() == filtro
-        ]
-
-    elif escolha == "2":
-        try:
-            filtro = int(input("Digite o ano de lançamento do filme: "))
-        except ValueError:
-            print("Ano inválido.")
-            return
-
-        resultados = [
-            filme for filme in filmes
-            if int(filme.ano_lancamento) == filtro
-        ]
-
-    else:
-        print("Opção inválida.")
-        return
-
-    if not resultados:
-        print("Nenhum filme encontrado.")
-        return
-
-    for filme in resultados:
-        print(f"""
-                {{
-                    "Nome": {filme.nome_filme},
-                    "Gênero": {filme.genero},
-                    "Ano de Lançamento": {filme.ano_lancamento},
-                    "Duração": {filme.duracao},
-                    "Nota": {filme.nota},
-                    "Sinopse": {filme.sinopse}
-                }}
-              """)
+CadastrarFilme()
+ListarFilmes()
